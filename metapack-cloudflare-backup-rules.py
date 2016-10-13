e1c2f3845a42782892010d94da0e3d6a59789649
Kashif Ali <kashif.ali@metapack.com>
2016-10-13 15:53:35 +0100

    created script to pull rules and back them up to a file, then merged the function into the main backup script. The main backup script creates two files, one with DNS records and one with the Rules for each top level domain:

--- metapack-cloudflare-backup-rules.py
+++ metapack-cloudflare-backup-rules.py
@@ -13,49 +13,6 @@ def get_zones(cf):
     return response
 
 
-def get_records_per_zone(cf, response):
-    total_pages = response['result_info']['total_pages']
-    page = 0
-    zones = []
-    sorted_dns_records = {}
-
-    while page <= total_pages:
-        page += 1
-        response = response = cf.zones.get(params={'page': page, 'per_page': 50})
-        zones.extend(response['result'])
-
-    for zone in zones:
-        zone_name = zone['name']
-        zone_id = zone['id']
-
-        print 'Getting DNS records for zone: %s' % zone_name
-        response = cf.zones.dns_records.get(zone_id, params={'per_page': 50})
-        total_pages = response['result_info']['total_pages']
-        page = 0
-        dns_records = []
-
-        while page <= total_pages:
-            page += 1
-            response = cf.zones.dns_records.get(zone_id, params={'page': page, 'per_page': 50})
-            dns_records.extend(response['result'])
-
-        record_list = []
-        for dns_records in sorted(dns_records, key=lambda v: v['name']):
-            single_record = {dns_records['name']: {
-                'id': dns_records['id'],
-                'type': dns_records['type'],
-                'ttl': dns_records['ttl'],
-                'content': dns_records['content'],
-                'proxied': dns_records['proxied'],
-                'proxiable': dns_records['proxiable']
-            }
-            }
-
-            record_list.append(single_record)
-            sorted_dns_records[zone_name] = record_list
-
-    return sorted_dns_records
-
 def get_rules_per_zone(cf, response):
     total_pages = response['result_info']['total_pages']
     page = 0
@@ -107,25 +64,21 @@ def get_rules_per_zone(cf, response):
     return sorted_rules
 
 
-
-def convert_to_yaml(records, type):
-    for zone_name in records:
-        #format = "%d-%m-%Y.%H:%M:%S"
+def convert_to_yaml(sorted_rules):
+    for zone_name in sorted_rules:
+        # format = "%d-%m-%Y.%H:%M:%S"
         # filename = '/tmp/cloudflare-backup-data-'  + zone_name + '-' + str(datetime.datetime.utcnow().strftime(format)) + '.yml'
-        filename = '/tmp/cloudflare-backup-' + type + '-data-' + zone_name + '.yml'
+        filename = '/tmp/cloudflare-backup-rule-' + zone_name + '.yml'
         with open(filename, 'w') as outfile:
-            yaml.safe_dump(records[zone_name], outfile, default_flow_style=False)
+            yaml.safe_dump(sorted_rules[zone_name], outfile, default_flow_style=False)
             print('created file for: ' + zone_name + ' : ' + filename)
 
 
 def main():
     cf = connect()
     zones = get_zones(cf)
-    sorted_dns_records = get_records_per_zone(cf, zones)
     sorted_rules = get_rules_per_zone(cf, zones)
-
-    convert_to_yaml(sorted_dns_records, 'dns-records')
-    convert_to_yaml(sorted_rules, 'rules')
+    convert_to_yaml(sorted_rules)
 
 
 if __name__ == '__main__':
